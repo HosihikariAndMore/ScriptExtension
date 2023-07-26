15efd0d9309dcf386fc91e612843c7325cac490a
LazuliKao <lazulikao@nbt.edu.cn>
2023-07-26 20:42:16 +0800

    init

---
+++ src/Hook/EnableScripting.cs
@@ -0,0 +1,17 @@
+﻿using Hosihikari.NativeInterop.Hook.ObjectOriented;
+using System.Runtime.InteropServices;
+using static Hosihikari.VanillaScript.Hook.EnableScriptingHook;
+
+namespace Hosihikari.VanillaScript.Hook;
+
+//force enable scripting
+internal class EnableScriptingHook : HookBase<HookDelegate>
+{
+    [return: MarshalAs(UnmanagedType.U1)]
+    public unsafe delegate bool HookDelegate(void* @this);
+
+    public EnableScriptingHook()
+        : base("_ZNK11Experiments8GametestEv") { }
+
+    public override unsafe HookDelegate HookedFunc => _ => true;
+}
---
+++ src/Main.cs
@@ -0,0 +1,15 @@
+﻿using Hosihikari.VanillaScript.Hook;
+using System.Runtime.CompilerServices;
+
+namespace Hosihikari.VanillaScript;
+
+public static class Main
+{
+#pragma warning disable CA2255
+    [ModuleInitializer]
+#pragma warning restore CA2255
+    public static void Init()
+    {
+        new EnableScriptingHook().Install();
+    }
+}
